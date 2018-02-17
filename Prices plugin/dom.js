2a9c8391b99eefc404ed43a331f79f3f1a3f1857
Catherine Koshchenko <catherine.pths@gmail.com>
2018-02-17 19:08:19 +0300

    Saving tags
    Now user can save tags with extension.

--- Prices plugin/dom.js
+++ Prices plugin/dom.js
@@ -1,6 +1,109 @@
 chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     if (request.action == "getHTML")
         sendResponse({ html: document.documentElement.outerHTML });
+    else if (request.action == "getSelection")
+        getSelected((res) => sendResponse(res));
     else
-        sendResponse({}); // Send nothing..
+        sendResponse({});
 });
+
+function getSelected(callback) {
+    var response = [];
+    if (typeof (window.getSelection()) != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            for (var i = 0; i < sel.rangeCount; i++) {
+                var html = "";
+                var container = document.createElement("div");
+                getSelectedArray(sel.getRangeAt(i), (arr) => response = response.concat(arr));
+            }
+        }
+    }
+    callback(response);
+}
+
+function getSelectedArray(range, callback) {
+    var nodes = [];
+    getRangeSelectedNodes(range, (arr) => nodes = arr);
+    var result = [];
+    for (var i = 0; i < nodes.length; i++) {
+        nodeToDict(nodes[i], (arr) => result = result.concat(arr))
+    }
+    var to = "hi";
+    callback(result);
+}
+
+function getRangeSelectedNodes(range, callback) {
+    var node = range.startContainer;
+    var endNode = range.endContainer;
+    if (node == endNode) {
+        callback([node]);
+        return;
+    }
+    var rangeNodes = [];
+    while (node && node != endNode) {
+        nextNode(node, (next) => {
+            node = next;
+            rangeNodes.push(next);
+        })
+    }
+    node = range.startContainer;
+    while (node && node != range.commonAncestorContainer) {
+        rangeNodes.unshift(node);
+        node = node.parentNode;
+    }
+    callback(rangeNodes);
+}
+
+function nodeToDict(node, callback) {
+    var result = [];
+    var path = "";
+    if (node.nodeType == 3 && node.data.replace(/\s*/g, '') != '') {
+        getPath(node, "", (txt) => path = txt);
+        var data = node.data.split(/\s+/);
+        for (var j = 0; j < data.length; j++) {
+            if (data[j].replace(/\s*/g, '') == '') continue;
+            result.push({ "text": data[j], "path": path });
+        }
+    }
+    callback(result);
+}
+
+function getPath(node, path, callback) {
+    const parent = node.parentElement;
+    if (parent.parentElement != null) {
+        if (parent.nodeName.localeCompare("SCRIPT") != 0) {
+            getWholeTag(parent, (tag) => {
+                getPath(parent, tag + path, (text) => callback(text));
+            });
+        } else {
+            getPath(parent, path, (text) => callback(text));
+        }
+    } else {
+        callback(path);
+    }
+};
+
+function nextNode(node, callback) {
+    if (node.hasChildNodes()) {
+        callback(node.firstChild);
+    } else {
+        while (node && !node.nextSibling) {
+            node = node.parentNode;
+        }
+        if (!node) {
+            callback(null);
+            return;
+        }
+        callback(node.nextSibling);
+    }
+}
+
+function getWholeTag(node, callback) {
+    var result = node.nodeName.toLowerCase();
+    if (node.hasAttribute("class")) {
+        result += ":" + node.getAttribute("class").replace(/\s*/g, '');
+    }
+    result += "/";
+    callback(result);
+}
--- Prices plugin/popup.js
+++ Prices plugin/popup.js
@@ -1,11 +1,3 @@
-function getCurrentTabUrl(callback) {
-    getCurrentTab((tab) => {
-        var url = tab.url;
-        console.assert(typeof url == 'string', 'tab.url should be a string');
-        callback(url);
-    });
-};
-
 function getCurrentTab(callback) {
     var queryInfo = {
         active: true,
@@ -33,19 +25,32 @@ function getWholeTag(node, callback) {
     callback(result);
 }
 
-function walkTextNodes(node, path, callback) {
+function getTagnameIfDefined(myText, myPath, tags, callback) {
+    for (var i = 0; i < tags.length; i++) {
+        var dict = tags[i];
+        if (dict["text"] == myText && dict["path"] == myPath) {
+            callback(dict["name"]);
+            return;
+        }
+    }
+    callback("");
+}
+
+function walkTextNodes(node, path, tags, callback) {
     const childNodes = node.childNodes;
     var result = "";
     for (var i = 0; i < childNodes.length; i++) {
         if (childNodes[i].nodeType == 1 && childNodes[i].nodeName.localeCompare("SCRIPT") != 0) {
             getWholeTag(childNodes[i], (tag) => {
-                walkTextNodes(childNodes[i], path + tag, (text) => result += text);
+                walkTextNodes(childNodes[i], path + tag, tags, (text) => result += text);
             });
         } else if (childNodes[i].nodeType == 3 && childNodes[i].data.replace(/\s*/g, '') != '') {
             var data = childNodes[i].data.split(/\s+/);
             for (var j = 0; j < data.length; j++) {
                 if (data[j].replace(/\s*/g, '') == '') continue;
-                result += "-1\t" + "node[0]=" + data[j] + "\tpath[0]=" + path + '\n';
+                var tagname = "";
+                getTagnameIfDefined(data[j], path, tags, (res) => tagname = res);
+                result += tagname + "\t" + "node[0]=" + data[j] + "\tpath[0]=" + path + '\n';
             }
         }
     }
@@ -55,31 +60,92 @@ function walkTextNodes(node, path, callback) {
 function getText(callback) {
     getCurrentTab((tab) => {
         chrome.tabs.sendRequest(tab.id, { action: "getHTML" }, function (response) {
-            htmlToDom(response.html, (dom) => {
-                dom.documentElement.normalize();
-                walkTextNodes(dom.documentElement, "", (text) => callback(text));
+            chrome.storage.local.get('tags', function (items) {
+                var tags = items.tags;
+                if (typeof (tags) == 'undefined') {
+                    tags = [];
+                }
+                htmlToDom(response.html, (dom) => {
+                    dom.documentElement.normalize();
+                    walkTextNodes(dom.documentElement, "", tags, (text) => callback(text));
+                });
             });
         });
     });
 }
 
-function download(filename, text) {
+function download(filename) {
     getText((text) => {
-        text = "__BOS__\n" + text + "__EOS__\n"
+        text = "___BEGIN___\n" + text + "___END___\n"
         var element = document.createElement('a');
         var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
         element.href = window.URL.createObjectURL(blob);
         element.download = filename + ".txt";
         element.click();
+        removeTags(function () { });
     });
 };
 
+function saveTag(tagname) {
+    getCurrentTab((tab) => {
+        chrome.tabs.sendRequest(tab.id, { action: "getSelection" }, function (responses) {
+            var dict = [];
+            for (var i = 0; i < responses.length; i++) {
+                var text = responses[i].text;
+                var path = responses[i].path;
+                dict.push({ "name": tagname, "text": text, "path": path });
+            }
+            chrome.storage.local.get("tags", function (items) {
+                if (typeof (items.tags) != 'undefined') {
+                    dict = dict.concat(items.tags);
+                }
+                chrome.storage.local.set({ "tags": dict }, function () {
+                    alert('Tag ' + tagname + ' saved');
+                });
+            });
+        });
+    });
+}
+
+function showTags() {
+    chrome.storage.local.get('tags', function (items) {
+        var result = "";
+        var dict = items.tags;
+        if (typeof (dict) == 'undefined') {
+            alert("NO TAGS SAVED!\n")
+            return;
+        }
+        for (var i = 0; i < dict.length; i++) {
+            result += "tag" + i + ": name = " + dict[i]["name"] + ", text = " + dict[i]["text"] + "\n";
+        }
+        alert(result);
+    });
+}
+
+function removeTags(callback) {
+    chrome.storage.local.clear(callback);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('button').addEventListener('click', () => {
-        getCurrentTabUrl((url) => {
-            var filename = document.getElementById('input').value;
-            if (filename == '') filename = "ali";
-            download(filename, "HELLO");
+    document.getElementById('save_html').addEventListener('click', () => {
+        var filename = document.getElementById('input_filename').value;
+        if (filename == '') filename = "avito";
+        download(filename);
+    });
+    document.getElementById('save_tag').addEventListener('click', () => {
+        var tagname = document.getElementById('input_tagname').value;
+        if (!tagname) {
+            alert('Error: No value specified');
+            return;
+        }
+        saveTag(tagname);
+    });
+    document.getElementById('show').addEventListener('click', () => {
+        showTags();
+    });
+    document.getElementById('delete').addEventListener('click', () => {
+        removeTags(function () {
+            alert('Removed all tags');
         });
     });
 });
